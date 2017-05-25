bb5771a2d990a40663556615384792a4d17692b0
Lotharan <dklotharan@hotmail.com>
2017-05-24 23:32:00 -0400

    corrected results output and display

--- app.js
+++ app.js
@@ -10,8 +10,8 @@ $(function() {
 
 function displayResults(input) {
 	var searchResults = '';
-	if (input.items) {
-		input.items.forEach(function(data) {
+	console.log(input);
+	if (input) {
 		 searchResults += '<div class="result_page">' +
 		'<nav class="navbar fixed-top navbar-toggleable-md navbar-inverse bg-inverse">' +
 			'<button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">' +
@@ -33,30 +33,30 @@ function displayResults(input) {
 			'</div>' +
 		'</nav>' +
 		'<div class="card mb-3">' +
-			'<img class="card-img-top" src="/' + data.url + '/" alt="Card image cap">' +
 			'<div class="card-block">' +
-				'<h4 class="card-title">' + data.title + '</h4>' +
-				'<p class="card-text">' + data.explaination + '</p>' +
-				'<p class="card-text"><small class="text-muted">Image Credit & Copyright:' + '' + data.copyright + '</small></p>' +
+				'<h4 class="card-title">' + input.title + '</h4>' +
+				'<p class="card-text">' + input.explanation + '</p>' +
+				'<p class="card-text"><small class="text-muted">Image Credit & Copyright:' + '' + input.copyright + '</small></p>' +
 			'</div>' +
+			'<img class="image_result" src="'+ input.url + '">'
 		'</div>' +
-		'</section>'});
+		'</section>';
+		console.log(input);
 	}
 	else {
-		searchResults += '<p>No results</p>';
+		searchResults += '<p>No results exist for this date please select another date</p>';
 	}
 
 	$('.js-search-results').html(searchResults);
 }
 
-$(function(){
-	$('form').submit(function(event){
-		event.preventDefault();
-		var userDate = $('#apod_date_js').val();
-		var formatDate = userDate.split('/');
-		var newDate = formatDate[2] + '-' + formatDate[0] + '-' + formatDate[1];
-		$.getJSON(`https://api.nasa.gov/planetary/apod?date=${newDate}&api_key=8a2IUlzhTscPWU51VzeTBLtwk9dyElTmRF4zJFRq`, function(data){ console.log(data)})
-	})
-
-})
+	$(function(){
+		$('form').submit(function(event){
+			event.preventDefault();
+			var userDate = $('#apod_date_js').val();
+			var formatDate = userDate.split('/');
+			var newDate = formatDate[2] + '-' + formatDate[0] + '-' + formatDate[1];
+			$.getJSON(`https://api.nasa.gov/planetary/apod?date=${newDate}&api_key=8a2IUlzhTscPWU51VzeTBLtwk9dyElTmRF4zJFRq`,displayResults)
+		});
+	});
 
