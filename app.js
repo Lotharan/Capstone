c0f6c794a24834b6aac805627200294dda83ec35
Lotharan <dklotharan@hotmail.com>
2017-05-31 22:24:54 -0400

    got links working

--- app.js
+++ app.js
@@ -17,17 +17,14 @@ function displayResults(input) {
 			'<button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">' +
 			'<span class="navbar-toggler-icon"></span>' +
 			'</button>' +
-			'<a class="navbar-brand" href="#">APOD</a>' +
+			'<a class="navbar-brand" href="https://apod.nasa.gov/apod/archivepix.html">APOD</a>' +
 			'<div class="collapse navbar-collapse" id="navbarNav">'+
 				'<ul class="navbar-nav">' +
-					'<li class="nav-item active">' +
-						'<a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>' +
+					'<li class="nav-item">' +
+						'<a class="nav-link js-search-again" href="#">Search Again</a>' +
 					'</li>' +
 					'<li class="nav-item">' +
-						'<a class="nav-link" href="#">Search Again</a>' +
-					'</li>' +
-					'<li class="nav-item">' +
-						'<a class="nav-link" href="#">More Info</a>' +
+						'<a class="nav-link" href="#">Back to top</a>' +
 					'</li>' +
 				'</ul>' +
 			'</div>' +
@@ -51,6 +48,9 @@ function displayResults(input) {
 }
 
 	$(function(){
+		$('body').on('click','.js-search-again',function(){
+			$('.js-search-results').empty();
+		})
 		$('form').submit(function(event){
 			event.preventDefault();
 			var userDate = $('#apod_date_js').val();
